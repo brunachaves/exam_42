16559c1981acb3c474788c58fb52ff592710900c
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-07-08 13:23:02 +0200

    str_capitalizer.c

---
+++ rank_02/level3/str_capitalizer.c
@@ -0,0 +1,77 @@
+/*
+Assignment name  : str_capitalizer
+Expected files   : str_capitalizer.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes one or several strings and, for each argument,
+capitalizes the first character of each word (If it's a letter, obviously),
+puts the rest in lowercase, and displays the result on the standard output,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string. If a word only has one letter, it must be
+capitalized.
+
+If there are no arguments, the progam must display \n.
+
+Example:
+
+$> ./str_capitalizer | cat -e
+$
+$> ./str_capitalizer "a FiRSt LiTTlE TESt" | cat -e
+A First Little Test$
+$> ./str_capitalizer "__SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+__second Test A Little Bit   Moar Complex$
+   But... This Is Not That Complex$
+     Okay, This Is The Last 1239809147801 But Not    The Least    T$
+$>
+*/
+
+#include <unistd.h>
+
+int is_space(char c)
+{
+    if (c == 9 || c == 32)
+        return (1);
+    return (0);
+}
+
+void str_cap(char *str)
+{
+    int i = 0;
+    int j = 0;
+
+    while(str[i])
+    {
+        if(str[i] >=  'A' && str[i] <=  'Z')
+            str[i] = str[i] + 32;
+        i++;
+    }
+    while(str[j])
+    {
+        if(str[j] >=  'a' && str[j] <=  'z')
+        {
+            if (j == 0  || is_space(str[j - 1]))
+                str[j] = str[j] - 32;
+        }
+        j++;
+    }
+    while(*str)
+    {
+        write(1, str, 1);
+        str++;
+    }
+}
+
+int main (int argc, char ** argv)
+{
+    int i = 1;
+
+    while(i < argc)
+    {
+        str_cap(argv[i]);
+        i++;
+    }
+    write(1, "\n", 1);
+}
