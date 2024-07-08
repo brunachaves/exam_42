2913e21908cb921d089b893ab85c96686bc09c3a
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-07-08 09:46:19 +0200

    epur

---
+++ rank_02/level3/epur_str.c
@@ -0,0 +1,62 @@
+/*
+Assignment name  : epur_str
+Expected files   : epur_str.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays this string with exactly one
+space between words, with no spaces or tabs either at the beginning or the end,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string.
+
+If the number of arguments is not 1, or if there are no words to display, the
+program displays \n.
+
+Example:
+
+$> ./epur_str "See? It's easy to print the same thing" | cat -e
+See? It's easy to print the same thing$
+$> ./epur_str " this        time it      will     be    more complex  . " | cat -e
+this time it will be more complex .$
+$> ./epur_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+$
+$> ./epur_str "" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int is_space(char c)
+{
+    if (c == 32 || c == 9)
+        return (1);
+    return (0);
+}
+
+void epur_str(char *str)
+{
+    while (is_space(*str))
+        str++;
+    while(*str)
+    {
+        while (!is_space(*str) && *str)
+        {
+            write(1, str, 1);
+            str++;
+        }
+        if (is_space(*str))
+            write (1, " ", 1);
+        while (is_space(*str))
+            str++;
+    }
+}
+
+int main (int argc, char **argv)
+{
+    if (argc == 2)
+        epur_str(argv[1]);
+    write(1, "\n", 1);
+}
